4e7982254a69f09cd89ea25f6d91629d47e55faa
JohnC0de <joaocarlos.m014@gmail.com>
2022-05-04 16:38:13 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import express from 'express'
+
+const app = express()
+
+app.get('/users', (req, res) => {
+  return res.send('Hello world')
+})
+
+app.listen(3333, () => console.log('Server started on port 3333'))
